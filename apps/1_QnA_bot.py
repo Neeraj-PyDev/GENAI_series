94d605c9a7b0e2ec87089a8a4ea79557ae058949
Keshri_Neeraj <124617625+Neeraj-PyDev@users.noreply.github.com>
2026-09-25 14:32:48 +0530

    Update 1_QnA_bot.py

--- apps/1_QnA_bot.py
+++ apps/1_QnA_bot.py
@@ -2,11 +2,46 @@
 
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
+import streamlit as st
 
 load_dotenv()
 
-llm = ChatGoogleGenerativeAI(
-    model="gemini-3.5-flash-lite")
-resp = llm.invoke("Who is PM of France?")
+llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash-lite")
 
-print(resp.content)
+# while True:
+#     query = input("User : ")
+#     if query.lower() in ['exit', 'clear', 'quit']:
+#         print('Goodbye !!')
+#         break
+#     res = llm.invoke(query)
+#     print('AI by Neer:', res.content, '\n')
+
+
+## Design the UI using streamlit title and markdown.
+
+st.title("🤓 Ask from BOT🤖 - *by @$tra*❤️")
+st.markdown("!!!!!!--------**QnA bot with Langchain & Gemini.👩‍🚀.Hope it will blooom the way of thoughts.--------!!!!!**")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+# store and print the earlier chats mesaages using streamlit session.
+
+for message in st.session_state.messages:
+    role = message["role"]
+    content = message["content"]
+    st.chat_message(role).markdown(content)
+
+query = st.chat_input("Ask Anything :: ")
+
+if query:
+    st.session_state.messages.append({"role":"user", "content":query})
+    st.chat_message("user").markdown(query)
+    resp = llm.invoke(query)
+    st.chat_message("ai").markdown(resp.content[0].get('text'))
+    st.session_state.messages.append({"role":"ai", "content":resp.content[0].get('text')})
+
+
+#----------------------------------
+
